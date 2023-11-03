d9f2c9ce11218093f27c70d01d64f9c88f44b023
Viktor Modroczký <v.modroczky@gmail.com>
2023-11-03 17:53:50 +0100

    Use explicit types instead of var

--- src/main/java/sk/fiit/Main.java
+++ src/main/java/sk/fiit/Main.java
@@ -2,16 +2,16 @@ package sk.fiit;
 
 public class Main {
     public static void main(String[] args)  {
-        var paypal = new PayPal();
+        PayPal paypal = new PayPal();
         paypal.pay(100);
         paypal.refund(100);
-        System.out.println("Class of paypal is " + paypal.getClass().getSimpleName() + ".");
+        System.out.println("Class of paypal object is " + paypal.getClass().getSimpleName() + ".");
 
-        var onlineCard = new OnlineCard();
+        OnlineCard onlineCard = new OnlineCard();
         onlineCard.pay(200);
         onlineCard.refund(200);
         /* If useSaferMethod in the ChangePaymentType aspect is set to true,
         the class printed out will be PayPal, otherwise it will be OnlineCardPayment. */
-        System.out.println("Class of onlineCard is " + onlineCard.getClass().getSimpleName() + ".");
+        System.out.println("Class of onlineCard object is " + onlineCard.getClass().getSimpleName() + ".");
     }
 }
